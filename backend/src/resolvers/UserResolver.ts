97f4063dd23c124258ebd69c1150210858bbfa07
Icaro Peretti <icaroperetti50@gmail.com>
2022-06-18 19:08:10 -0300

    Adding update and delete

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -2,6 +2,10 @@ import crypto from "crypto";
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../models/User";
 
+// Query: Buscar dados
+
+// Mutation: Criat,alterar ou deletar
+
 @Resolver()
 export class UserResolver {
   private data: User[] = [];
@@ -16,6 +20,32 @@ export class UserResolver {
     const user = { id: crypto.randomUUID(), name };
 
     this.data.push(user);
+
+    return user;
+  }
+
+  @Mutation(() => User)
+  async updateUser(@Arg("id") id: string, @Arg("name") name: string) {
+    const user = this.data.find((user) => user.id === id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    user.name = name;
+
+    return user;
+  }
+
+  @Mutation(() => User)
+  async deleteUser(@Arg("id") id: string) {
+    const user = this.data.find((user) => user.id === id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    this.data = this.data.filter((user) => user.id !== id);
 
     return user;
   }
